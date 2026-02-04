5bebb27b1c5bb2215be766c7049e211a73a7d18a
matthias <matthias.greil2411@gmail.com>
2026-02-04 14:40:25 +0100

    help

--- robots/2026-spring/axht-3085/cube-bot/src/missions/setup_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/setup_mission.py
@@ -10,8 +10,7 @@ class SetupMission(Mission):
             servo_pom_arm_start(),
             servo_pom_grab_close(),
 
-            calibrate_distance(),
-            calibrate_sensors(calibration_time=5),
+            calibrate_distance(calibrate_light_sensors=True),
             wait_for_button(),
             #calibrate_wait_for_light(Defs.wait_for_light_sensor),
         ])
