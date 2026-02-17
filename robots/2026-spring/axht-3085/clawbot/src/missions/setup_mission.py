7b1d0bec3efafbd86a455c2a66759f25376bbafe
Raccoon Auto History <raccoon@local>
2026-02-17 13:54:13 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.237:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M src/hardware/.raccoon_codegen_cache.json
    - M src/hardware/defs.py
    - M src/hardware/robot.py
    - M src/missions/setup_mission.py

--- robots/2026-spring/axht-3085/clawbot/src/missions/setup_mission.py
+++ robots/2026-spring/axht-3085/clawbot/src/missions/setup_mission.py
@@ -6,12 +6,14 @@ from src.steps.servo_steps import *
 class SetupMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
+            drive_forward(cm=50),
+            turn_left(degrees=90),
             #set servos to start pos
-            servo_pom_arm_start(),
-            servo_pom_grab_start(),
-            servo_shild_down(),
-
-            calibrate(distance_cm=50),
+            # servo_pom_arm_start(),
+            # servo_pom_grab_start(),
+            # servo_shild_down(),
+            #
+            # calibrate(distance_cm=50),
             wait_for_button(),
 
             #calibrate_wait_for_light(Defs.wait_for_light_sensor),
