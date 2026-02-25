f0d32034e122fa564219d7af80ac53f84d0f45fe
Raccoon Auto History <raccoon@local>
2026-02-25 18:16:53 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.237:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 1 files (+0 ~1 -0)
    
    Changed files:
    - M src/missions/m01_setup_mission.py

--- robots/2026-spring/axht-3085/packing-bot/src/missions/m01_setup_mission.py
+++ robots/2026-spring/axht-3085/packing-bot/src/missions/m01_setup_mission.py
@@ -2,7 +2,6 @@ from libstp import *
 from src.hardware.defs import *
 from src.steps.light_sensor_steps import frontside_forward_lineup_on_black
 from src.steps.servo_steps import *
-from src.steps.single_line_follow import follow_line_single_right_edge
 
 
 class M01SetupMission(Mission):
