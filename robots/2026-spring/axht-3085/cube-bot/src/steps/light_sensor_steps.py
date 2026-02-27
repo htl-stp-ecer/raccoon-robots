7bda0487c20a347c7a593cd6dd8a096fdefdd441
Raccoon Auto History <raccoon@local>
2026-02-27 20:06:18 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.237:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 1 files (+0 ~1 -0)
    
    Changed files:
    - M src/steps/light_sensor_steps.py

--- robots/2026-spring/axht-3085/cube-bot/src/steps/light_sensor_steps.py
+++ robots/2026-spring/axht-3085/cube-bot/src/steps/light_sensor_steps.py
@@ -44,15 +44,7 @@ def frontside_line_follow_right_edge(cm, speed = 1.0):
     return follow_line_single_right_edge(Defs.front_right_light_sensor, cm, speed)
 
 @dsl
-def left_starfe_lineup_on_black(threshold = 0.7):
-    return strafe_left_lineup_on_black(Defs.front_left_light_sensor,
-                                       Defs.rear_left_light_sensor,
-                                       speed = 1.0,
-                                       confidence_threshold=threshold)
-
-@dsl
 def left_starfe_until_black(threshold = 0.7, speed = 1.0):
-    return strafe_left_until_black([Defs.front_left_light_sensor,
-                                       Defs.rear_left_light_sensor],
+    return strafe_left_until_black(Defs.front_left_light_sensor,
                                        speed = speed,
                                        confidence_threshold=threshold)
