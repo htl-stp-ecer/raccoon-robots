f6b1806eaa7d89bb39c2b00b798ec9ca7e06c27d
Raccoon Auto History <raccoon@local>
2026-02-27 20:15:06 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.237:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M src/hardware/.raccoon_codegen_cache.json
    - M src/hardware/defs.py
    - M src/hardware/robot.py
    - M src/missions/m01_setup_mission.py

--- robots/2026-spring/axht-3085/clawbot/src/missions/m01_setup_mission.py
+++ robots/2026-spring/axht-3085/clawbot/src/missions/m01_setup_mission.py
@@ -1,6 +1,6 @@
 from libstp import *
 from src.hardware.defs import *
-from src.steps.light_sensor_steps import frontside_forward_lineup_on_black
+from src.steps.light_sensor_steps import frontside_forward_lineup_on_black, frontside_line_follow_right_edge
 from src.steps.servo_steps import *
 from src.steps.single_line_follow import follow_line_single_right_edge
 
