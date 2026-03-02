e6151d6413526dd5d510d4db16ba3480ef3f3f27
Raccoon Auto History <raccoon@local>
2026-03-02 19:33:40 +0100

    chore(sync): pre-push snapshot
    Direction: push
    Target: pi@192.168.100.237:/home/pi/programs/322a6cb2-b54d-4ad2-bb55-14d157403ae7
    Summary: 4 files (+0 ~4 -0)
    
    Changed files:
    - M src/hardware/.raccoon_codegen_cache.json
    - M src/hardware/defs.py
    - M src/hardware/robot.py
    - M src/missions/m03_drive_down_acces_ramp_mission.py

--- robots/2026-spring/axht-3085/clawbot/src/hardware/defs.py
+++ robots/2026-spring/axht-3085/clawbot/src/hardware/defs.py
@@ -1,7 +1,7 @@
 """
 ===========================================================
  Project:   PackingBot
- Generated: 2026-03-02 17:50:14
+ Generated: 2026-03-02 18:30:07
 ===========================================================
 
 Authors:
@@ -23,28 +23,28 @@ class Defs:
         port=1,
         inverted=False,
         calibration=MotorCalibration(
-            ticks_to_rad=1.5525702234560802e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.547139670297372e-05, vel_lpf_alpha=1.0
         ),
     )
     front_right_motor = Motor(
         port=0,
         inverted=True,
         calibration=MotorCalibration(
-            ticks_to_rad=1.5384471097608305e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.533617919179291e-05, vel_lpf_alpha=1.0
         ),
     )
     rear_left_motor = Motor(
         port=2,
         inverted=False,
         calibration=MotorCalibration(
-            ticks_to_rad=1.5611299324615e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.5634855096111943e-05, vel_lpf_alpha=1.0
         ),
     )
     rear_right_motor = Motor(
         port=3,
         inverted=True,
         calibration=MotorCalibration(
-            ticks_to_rad=1.5887515340491162e-05, vel_lpf_alpha=1.0
+            ticks_to_rad=1.586310861275123e-05, vel_lpf_alpha=1.0
         ),
     )
     rear_right_light_sensor = IRSensor(port=1)
