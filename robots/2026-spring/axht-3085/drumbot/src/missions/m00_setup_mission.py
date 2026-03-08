1bfacdf55394191a05bf94692701aa72bda9681e
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-03-08 10:13:24 +0100

    Updated robot parameters and refined mission sequences

--- robots/2026-spring/axht-3085/drumbot/src/missions/m00_setup_mission.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m00_setup_mission.py
@@ -1,9 +1,5 @@
-from src.steps.drum_collector import calibrate_drum_collector
-from src.steps.drum_lifting_step import *
-from src.steps.drum_pusher_servo import open_drum_pusher
-from src.steps.range_finder import calibrate_range_finder
-from src.steps.range_finder import turn_to_peak
-
+from libstp.mission.api import Mission
+from libstp.step.sequential import Sequential, seq
 
 class M00SetupMission(Mission):
     def sequence(self) -> Sequential:
@@ -12,12 +8,4 @@ class M00SetupMission(Mission):
             calibrate_range_finder(),
             wait_for_button(),
             turn_to_peak()
-            # open_drum_pusher(),
-            # drum_lifting_up(slow_mode=False),
-            # calibrate(distance_cm=50, exclude_ir_sensors=[
-            #     Defs.wait_for_light_sensor,
-            #     Defs.drum_light_sensor
-            # ]),
-            # drum_lifting_down(),
-            # calibrate_drum_collector(calibration_time=2.0),
         ])
--- robots/2026-spring/axht-3085/drumbot/src/missions/m03_drive_to_pipe.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m03_drive_to_pipe.py
@@ -1,24 +1,26 @@
 from libstp import *
+
+from src.hardware.defs import Defs
 from src.steps.drum_collector import drum_retreat
 from src.steps.drum_lifting_step import drum_lifting_up, dispense_drums
+from src.steps.range_finder.scan_sweep_step import ScanSweepStep
 
 
 class M03DriveToPipe(Mission):
    def sequence(self) -> Sequential:
        return seq([
            dispense_drums(),
-           drive_backward(35,1),
-           turn_right(90,1),
-           drive_backward(15,1),
-           drive_forward(11,1),
-           turn_right(90,1),
-           drive_forward(43,1),
+           drive_backward(30,1),
+           turn_right(180,1),
+           forward_lineup_on_black(Defs.front_left_ir_sensor, Defs.front_right_ir_sensor),
+           drive_forward(27.5,1),
 
-           drum_retreat(),
-           drum_retreat(),
-           drum_retreat(),
-           drum_retreat(),
-           drum_retreat(),
+           ScanSweepStep(),
+           turn_left(15,1),
+           drive_forward(8,1),
+
+
+
            drum_retreat(),
            drum_retreat(),
            drum_retreat(),
