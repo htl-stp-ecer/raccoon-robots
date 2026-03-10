f92d0bf563c3d59f471ce781baf271934297535c
matthias <matthias.greil2411@gmail.com>
2026-03-10 20:44:26 +0100

    words sometimes until end of the 6. Mission

--- robots/2026-spring/axht-3085/cube-bot/src/missions/m06_collect_last_poms_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m06_collect_last_poms_mission.py
@@ -6,10 +6,28 @@ class M06CollectLastPomsMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
             drive_forward(5.0, 1.0),
-            Defs.front.strafe_left_until_black(speed=0.3, threshold=0.3),
+            strafe_left(speed=0.5).until(on_white(Defs.front.left, threshold=0.3)),
 
-            strafe_right(speed=0.3).until(on_white(Defs.front.left, threshold=0.3)),
+            strafe_right(speed=1.0, cm=6),
 
-            turn_to_heading(0, 1.0),
+            #align for poms
+            turn_to_heading(-90, 1.0),
+            Defs.pom_arm.down(),  # put down claw so we can strafe easier
+            wall_align_backward(1.0, 0.3, 0.3, 3),
             Defs.pom_grab.wide_open(),
+            wait_for_seconds(1),
+
+            #collect poms
+            parallel(
+                drive_forward(50, 1.0),
+                seq([
+                    wait_until_distance(5),
+                    Defs.pom_grab.slightly_open(), #not the normal slightliy open (zahnradspiel ist real)
+                    wait_until_distance(20),
+                    Defs.pom_grab.wide_open(),
+                ]),
+            ),
+
+            Defs.pom_grab.closed(),
+            Defs.pom_arm.high_up(),
         ])
