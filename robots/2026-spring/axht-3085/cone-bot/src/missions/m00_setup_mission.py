54471b173241e1ee560f965ce375a5566e9021fc
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-03-14 12:23:23 +0100

    Add initial implementation of missions, hardware configurations, and setup routines.

--- robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
+++ robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
@@ -1,8 +1,15 @@
-from libstp.mission.api import Mission
-from libstp.step.sequential import Sequential, seq
+from libstp import *
+
+from src.hardware.defs import Defs
+from src.steps.cone_container_steps import down_cone_container, up_cone_container
+
 
 class M00SetupMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-            calibrate(distance=50)
+            Defs.claw_servo.closed(),
+            Defs.cone_arm_servo.up(),
+            down_cone_container(),
+            calibrate(distance_cm=50),
+            up_cone_container(),
         ])
--- robots/2026-spring/axht-3085/cone-bot/src/missions/m01_drive_to_cone_mission.py
+++ robots/2026-spring/axht-3085/cone-bot/src/missions/m01_drive_to_cone_mission.py
@@ -3,5 +3,22 @@ from libstp import *
 class M01DriveToConeMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-             drive_forward(73),
+            turn_right(deg=65),
+            drive_forward(cm=41),
+            # turn_right(deg=90),
+            # drive_forward(cm=46),
+            # forward_lineup_on_black(),
+            # drive_forward(cm=41),
+            # forward_lineup_on_black(),
+            # turn_left(deg=180),
+            # drive_forward(cm=93),
+            # turn_left(deg=91),
+            # drive_forward(cm=5),
+            # forward_lineup_on_black(),
+            # turn_right(deg=1),
+            # drive_forward(cm=34),
+            # turn_left(deg=91),
+            # drive_forward(cm=3),
+            # forward_lineup_on_black(),
+            # follow_line(cm=146)
         ])
