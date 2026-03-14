841f49f120f161a9a2a116cb47c4dd1d91df68b6
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-03-14 18:24:07 +0100

    Update mission setup and hardware configurations; adjust PID, motor calibration, and angular dynamics.

--- robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
+++ robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
@@ -10,10 +10,5 @@ class M00SetupMission(Mission):
             Defs.claw_servo.closed(),
             Defs.cone_arm_servo.up(),
             calibrate(distance_cm=50),
-            # Defs.cone_arm_servo.down()
-            # auto_tune(
-            #     vel_axes=["vx", "wz"],
-            #     characterize_axes=["forward", "angular"],
-            #     motion_axes=["distance", "heading"],
-            # ),
+            Defs.cone_arm_servo.down()
         ])
