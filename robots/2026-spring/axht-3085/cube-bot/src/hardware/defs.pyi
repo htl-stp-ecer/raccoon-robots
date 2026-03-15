493f1d9d8a718f8357ed0fcb7ceccf194185ddc6
Matthias Greil <matthias.greil2411@gmail.com>
2026-03-15 19:02:33 +0100

    should works until m09_drop_maching_poms! needs a lot of work

--- robots/2026-spring/axht-3085/cube-bot/src/hardware/defs.pyi
+++ robots/2026-spring/axht-3085/cube-bot/src/hardware/defs.pyi
@@ -5,44 +5,51 @@ from libstp import DigitalSensor
 from libstp import IMU as Imu
 from libstp import IRSensor
 from libstp import Motor
-from libstp.step import Step
 from libstp.step.motion.sensor_group import SensorGroup
-from libstp.step.servo.preset import ServoPreset
+from libstp.step.servo.preset import ServoPreset, _PresetPosition
 from typing import List
 
 
 class _ShildPreset(ServoPreset):
-    def up(self, speed: float | None = None) -> Step: ...
-    def down(self, speed: float | None = None) -> Step: ...
+    up: _PresetPosition
+    down: _PresetPosition
+    above_pasked: _PresetPosition
+    grab_pasked: _PresetPosition
+
     @property
     def device(self) -> "Servo": ...
 
 
 class _PomArmPreset(ServoPreset):
-    def down(self, speed: float | None = None) -> Step: ...
-    def above_pom(self, speed: float | None = None) -> Step: ...
-    def up(self, speed: float | None = None) -> Step: ...
-    def start(self, speed: float | None = None) -> Step: ...
-    def high_up(self, speed: float | None = None) -> Step: ...
+    down: _PresetPosition
+    above_pom: _PresetPosition
+    above_basket: _PresetPosition
+    up: _PresetPosition
+    start: _PresetPosition
+    high_up: _PresetPosition
+
     @property
     def device(self) -> "Servo": ...
 
 
 class _ShildGraberPreset(ServoPreset):
-    def open(self, speed: float | None = None) -> Step: ...
-    def wide_open(self, speed: float | None = None) -> Step: ...
-    def closed(self, speed: float | None = None) -> Step: ...
+    open: _PresetPosition
+    wide_open: _PresetPosition
+    closed: _PresetPosition
+
     @property
     def device(self) -> "Servo": ...
 
 
 class _PomGrabPreset(ServoPreset):
-    def closed(self, speed: float | None = None) -> Step: ...
-    def start(self, speed: float | None = None) -> Step: ...
-    def pom_width(self, speed: float | None = None) -> Step: ...
-    def slightly_open(self, speed: float | None = None) -> Step: ...
-    def open(self, speed: float | None = None) -> Step: ...
-    def wide_open(self, speed: float | None = None) -> Step: ...
+    closed: _PresetPosition
+    start: _PresetPosition
+    pom_width: _PresetPosition
+    slightly_open: _PresetPosition
+    open: _PresetPosition
+    wide_open: _PresetPosition
+    magic_val_for_m06: _PresetPosition
+
     @property
     def device(self) -> "Servo": ...
 
--- robots/2026-spring/axht-3085/cube-bot/src/missions/m06_collect_last_poms_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m06_collect_last_poms_mission.py
@@ -13,22 +13,26 @@ class M06CollectLastPomsMission(Mission):
 
             #align for poms
             turn_to_heading(-90, 1.0),
-            Defs.pom_arm.down(),  # put down claw so we can strafe easier
+            Defs.pom_arm.down(),
             wait_for_seconds(0.5),
             wall_align_backward(1.0, 0.4, 0.0, 2),
             Defs.pom_grab.wide_open(),
             wait_for_seconds(1),
 
             #collect poms
+            drive_forward(11, 1.0),
+            drive_backward(1.0, 1.0),
+            Defs.pom_grab.pom_width(100),
+            Defs.pom_grab.magic_val_for_m06(100),
             parallel(
-                drive_forward(50, 1.0),
+                drive_forward(45, 1.0),
                 seq([
-                    Defs.pom_grab.pom_width(200), #not the normal slightliy open (zahnradspiel ist real)
-                    wait_until_distance(15),
+                    wait_until_distance(5),
                     Defs.pom_grab.wide_open(),
                 ]),
             ),
 
             Defs.pom_grab.closed(),
+            wait_for_seconds(0.3),
             Defs.pom_arm.high_up(),
         ])
