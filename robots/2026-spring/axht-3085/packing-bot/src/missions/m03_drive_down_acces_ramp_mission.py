89bbd6ec7dbb1bc7776183ed31410b57b06d142c
Matthias Greil <matthias.greil2411@gmail.com>
2026-03-21 12:10:15 +0100

    fixed the m06 so that we take tow less poms!

--- robots/2026-spring/axht-3085/packing-bot/src/missions/m03_drive_down_acces_ramp_mission.py
+++ robots/2026-spring/axht-3085/packing-bot/src/missions/m03_drive_down_acces_ramp_mission.py
@@ -8,12 +8,16 @@ class M03DriveDownAccesRampMission(Mission):
         return seq([
             switch_calibration_set("default"),
 
-            turn_to_heading(180, 1.0),
+            #turn_to_heading(180, 1.0),
+            turn_right(90), #FIXME do the turn to heading i am only heraf for testing!
+            Defs.shild.up(), #FIXME remove only hear for testing
             wall_align_backward(1.0, 0.4, 0.1, 1.0),
             mark_heading_reference(),  # mark heading for use in drive down acess ramp
 
-            Defs.rear.strafe_right_until_black(),
-            strafe_right(5, 1.0),
+            strafe_right().until(on_black(Defs.rear.right)),
+            strafe_left(speed=0.5).until(on_white(Defs.rear.right)),
+            strafe_right(speed=0.3).until(on_black(Defs.rear.right)),
+            #strafe_right(3, 1.0),
             turn_to_heading(0, 1.0),
             wall_align_backward(1.0, 0.4, 0.0, 0.3),
         ])
