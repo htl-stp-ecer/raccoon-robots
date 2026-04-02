cf436b68a71e9632c90f74a31e95020926d084eb
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-04-02 18:11:51 +0200

    Refactor and enhance missions; update servo configurations, motor calibrations, and sequence logic

--- robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
+++ robots/2026-spring/axht-3085/cone-bot/src/missions/m00_setup_mission.py
@@ -10,17 +10,6 @@ class M00SetupMission(Mission):
             Defs.claw_servo.closed(),
             Defs.cone_arm_servo.up(),
             calibrate(distance_cm=50),
-            loop_forever(
-                seq([
-                    wait_for_button(),
-                    forward_single_lineup(
-                        Defs.front.right,
-                        entry_threshold=0.9,
-                        exit_threshold=0.85,
-                        correction_side=CorrectionSide.RIGHT,
-                        forward_speed=0.5,
-                    ),
-                ])
-            ),
+
             Defs.cone_arm_servo.down()
         ])
--- robots/2026-spring/axht-3085/cone-bot/src/missions/m02_collect_cone_mission.py
+++ robots/2026-spring/axht-3085/cone-bot/src/missions/m02_collect_cone_mission.py
@@ -7,14 +7,14 @@ class M02CollectConeMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
             drive_backward(10),
-            turn_left(30),
-            drive_backward(5),
+            turn_left(25),
+            drive_backward(6),
             Defs.claw_servo.open(),
             Defs.cone_arm_servo.down(),
             Defs.claw_servo.closed(120),
-            Defs.cone_arm_servo.up(120),
-            Defs.claw_servo.open(60),
-            drive_forward(5),
-            turn_right(30),
+            Defs.cone_arm_servo.up(140),
+            Defs.claw_servo.half_open(100),
+            drive_forward(9),
+            turn_right(25),
             Defs.front.drive_over_line(),
         ])
