80ccadb72ccbdb30b0fd1b6e42117222aa86a396
Tobias Madlberger <tobias.madlberger@gmail.com>
2026-04-03 19:38:12 +0200

    Replaced `eject_drums` with `eject_nearest_color` in M04 mission to simplify sequence logic.

--- robots/2026-spring/axht-3085/drumbot/src/missions/m04_eject_drums_mission.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m04_eject_drums_mission.py
@@ -3,16 +3,9 @@ from libstp import *
 from src.steps.drum_collector import eject_nearest_color
 
 
-@dsl
-def eject_drums() -> Sequential:
-    return seq([
-        eject_nearest_color(),
-    ])
-
-
 class M04EjectDrumsMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-            #wait_for_button(),
-            eject_drums(),
+            # wait_for_button(),
+            eject_nearest_color(),
         ])
