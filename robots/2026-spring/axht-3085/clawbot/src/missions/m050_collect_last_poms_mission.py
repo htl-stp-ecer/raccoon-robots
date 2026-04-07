a03e07d6cd117886d0ff027b17bc8e236f428f62
Matthias Greil <matthias.greil2411@gmail.com>
2026-04-07 19:20:20 +0200

    returning baskets works

--- robots/2026-spring/axht-3085/clawbot/src/missions/m050_collect_last_poms_mission.py
+++ robots/2026-spring/axht-3085/clawbot/src/missions/m050_collect_last_poms_mission.py
@@ -32,8 +32,9 @@ class M050CollectLastPomsMission(Mission):
                     Defs.pom_grab.wide_open(),
 
                     #make sure we collect ALL lost poms
-                    wait_until_distance(cm=33),
-                    Defs.pom_grab.closed(),
+                    wait_until_distance(cm=35),
+                    Defs.pom_grab.m05_slightly_open(),
+                    wait_for_seconds(0.5),
                     Defs.pom_grab.slightly_open(),
                 ])
             ),
