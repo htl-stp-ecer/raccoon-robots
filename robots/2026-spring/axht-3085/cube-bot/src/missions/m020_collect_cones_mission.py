b6086636fb879b01ecc6c150e1ec4764d6449fe8
Matthias Greil <matthias.greil2411@gmail.com>
2026-05-12 19:52:38 +0200

    grabin the basket works now

--- robots/2026-spring/axht-3085/cube-bot/src/missions/m020_collect_cones_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m020_collect_cones_mission.py
@@ -3,63 +3,81 @@ from raccoon import *
 from src.hardware.defs import Defs
 from src.kinematics.arm import arm
 
+
 def line_follow():
     return strafe_follow_line_single(
         Defs.front_left_light_sensor,
         speed=1,
         side=LineSide.RIGHT,
-        kp=0.6,
-        ki=0.1,
+        kp=0.4,
+        ki=0.2,
         kd=0.0,
     )
+
+def drop_cone_into_holder(base_angle: float):
+    return seq([
+        # position over holder
+        #arm.move_angles(base_angle, 90, 70),
+
+        #drop cone
+        arm.move_angles(base_angle, 90, 90),
+        Defs.arm_claw.p45deg(),
+
+        #make arm ready for next move
+        #arm.move_angles(base_angle, 90, 60),
+        #Defs.arm_claw.closed(),
+    ])
 
 class M020CollectConesMission(Mission):
     def sequence(self) -> Sequential:
         return seq([
-            strafe_left().until( #TODO: use a sidways line follow
+            strafe_left().until(  # TODO: use a sidways line follow
                 over_line(Defs.rear_left_light_sensor)
-                #+ over_line(Defs.front_right_light_sensor)
-                +after_cm(23),
+                # + over_line(Defs.front_right_light_sensor)
+                + after_cm(23),
             ),
-            #positon arm to grab first cone
+            # positon arm to grab first cone
             background(
                 step=parallel(
-                    arm.move_to(35, -5, 5).forearm(angle=0, precision=1),
+                    arm.move_angles(-12, -10, 20),
                     Defs.arm_claw.p90deg(),
                 )
             ),
-            #drive to line
-            drive_angle(angle_deg=-55).until(
+            # drive to line
+            drive_angle(angle_deg=-75).until(
                 over_line(Defs.rear_left_light_sensor)
                 + over_line(Defs.front_left_light_sensor)
             ),
 
             turn_to_heading_right(0),
-            #follow the line and grab the cones in parralel
+            # follow the line and grab the cones in parralel
             parallel(
                 line_follow().until(
-                    over_line(Defs.front_right_light_sensor)
-                    + over_line(Defs.front_right_light_sensor)
+                    over_line(Defs.front.right)
+                    + over_line(Defs.front.right)
+                    + after_cm(5) #make sure we are over the line (for strafing)
                 ),
                 seq([
-                    #wati until we reach the first T --> grab --> then drop into holder
-                    wait_for(on_black(Defs.front_right_light_sensor)),
-                    Defs.arm_claw.closed(),
-                    arm.move_angles(-11, 92, 85),
-                    Defs.arm_claw.p45deg(),
-                    wait_for_seconds(1), #wait so the cone drops for 100%
+                    # wait until we reach the first T --> grab --> then drop into holder
+                    wait_for(on_black(Defs.front.right)),
+                    Defs.arm_claw.closed(), #grab cone
+                    drop_cone_into_holder(base_angle=-10),
 
-                    #position arm so we can grab the second conde
+                    # position arm so we can grab the second conde
                     parallel(
-                        arm.move_to(35, -5, 5).forearm(angle=0, precision=0.8),
+                        arm.move_angles(-12, -10, 20),
                         Defs.arm_claw.p90deg(),
                     ),
 
-                    #wati until we reach the second T --> grab --> then drop into holder
-                    wait_for(on_black(Defs.front_right_light_sensor)),
-                    Defs.arm_claw.closed(),
-                    arm.move_angles(-40, 100, 75),
-                    Defs.arm_claw.p45deg(),
+                    # wait until we reach the second T --> grab --> then drop into holder
+                    wait_for(on_black(Defs.front.right)),
+                    background(
+                        step=seq([
+                            Defs.arm_claw.closed(),#grab cone
+                            drop_cone_into_holder(base_angle=25),
+                        ]),
+                        name="drop_cone",
+                    ),
                 ]),
 
             ),
---
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m030_collect_drums_mission.py
@@ -0,0 +1,40 @@
+from raccoon import *
+
+from src.hardware.defs import Defs
+from src.kinematics.arm import arm
+from src.steps.arm_steps import arm_grab_basket
+
+def line_follow():
+    return strafe_follow_line_single(
+        Defs.front.right,
+        speed=1,
+        side=LineSide.RIGHT,
+        kp=0.4,
+        ki=0.2,
+        kd=0.0,
+    )
+
+class M030CollectDrumsMission(Mission):
+    def sequence(self) -> Sequential:
+        return seq([
+            parallel(
+                seq([
+                    turn_to_heading_left(0),
+                    strafe_left().until(
+                        over_line(Defs.front.right)
+                    ),
+                    turn_to_heading_left(0),
+                ]),
+                seq([
+                    wait_for_background("drop_cone"),
+                    arm_grab_basket(),
+                    arm.move_angles(-90, 90, -45),
+                    arm.move_angles(-90, 45, -30),
+                ]),
+            ),
+
+            line_follow().until(
+                over_line(Defs.rear.left)
+                + after_cm(5)
+            ),
+        ])
