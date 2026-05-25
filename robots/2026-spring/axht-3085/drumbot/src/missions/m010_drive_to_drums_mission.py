87d395b986a1c58630fc1ef65ac81e6938e6b433
MarioS271 <marioerik.scholze@gmail.com>
2026-05-25 16:56:48 +0200

    clean up m010 a little

--- robots/2026-spring/axht-3085/drumbot/src/missions/m010_drive_to_drums_mission.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m010_drive_to_drums_mission.py
@@ -1,8 +1,6 @@
 from raccoon import *
-
 from src.hardware.defs import *
 from src.steps.drum_lifting_step import *
-
 
 class M010DriveToDrumsMission(Mission):
     def sequence(self) -> Sequential:
@@ -53,58 +51,3 @@ class M010DriveToDrumsMission(Mission):
             wall_align_forward(accel_threshold=0.3),
             mark_heading_reference(),
         ])
-
-
-        # return seq([
-        #     mark_heading_reference(
-        #         origin_offset_deg=90,
-        #     ),
-        #
-        #     background(
-        #         step=seq([
-        #             # push orange pom away
-        #             wait_for_seconds(0.1),
-        #             Defs.pom_remover_servo.orange_pom_removel(),
-        #             fully_disable_servos(),
-        #         ])
-        #     ),
-        #
-        #     #wait_for_seconds(0.3),
-        #
-        #     smooth_path(
-        #         turn_to_heading_right(16.7),  # over rotate so we push the sorted poms better value before 14
-        #         turn_to_heading_right(17.8),  # magic value, so we don't hit the extenal loading dock
-        #         parallel(
-        #             seq([
-        #                 drive_forward().until(
-        #                     after_cm(20) +
-        #                     over_line(Defs.front_right_ir_sensor)
-        #                 ),
-        #             ]),
-        #             background(
-        #                 step=seq([
-        #                     wait_until_distance(5),
-        #                     Defs.pom_remover_servo.right(),
-        #                     fully_disable_servos(),
-        #                     wait_until_distance(29),
-        #                     Defs.pom_remover_servo.left(),
-        #                     #fully_disable_servos(),
-        #                     #wait_until_distance(22),
-        #                     drum_recover_from_over_limit(Defs.lift_drums_servo.up),
-        #                 ]),
-        #             ),
-        #         ),
-        #
-        #         # turn straight
-        #         turn_to_heading_right(0),
-        #
-        #         parallel(
-        #             drive_forward(cm=27, heading=0),
-        #             Defs.drum_pusher_servo.open(),
-        #             seq([
-        #                 wait_until_distance(12), #only a good guess of distance
-        #                 drum_lifting_down(slow_mode=False),
-        #             ]),
-        #         ),
-        #     ),
-        # ])
