7a4e8f69828a4ec0e34248ef3fc796c91fe3f664
MarioS271 <marioerik.scholze@gmail.com>
2026-06-12 20:46:37 +0200

    fix(m010): correctly moves to drum pickup pos now

--- robots/2026-spring/axht-3085/drumbot/src/missions/m010_drive_to_drums_mission.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m010_drive_to_drums_mission.py
@@ -8,28 +8,22 @@ class M010DriveToDrumsMission(Mission):
         return seq([
             mark_heading_reference(),
 
-            #drive_backward(heading=90).until(
-            #    on_black(Defs.rear_left_ir_sensor)
-            #),
-
-            #turn_right(45),
-            #drive_forward(14),
-            #parallel(
-            #    turn_to_heading_left(0),
-            #    Defs.pom_remover_servo.right(),
-            #),
-
+            # drive out of starting box
             drive_forward(heading=0).until(
                 over_line(Defs.front_right_ir_sensor)
             ),
+
+            # turn drive turn
             turn_right(45),
-            drive_forward(cm=15, speed=0.6),
+            drive_forward(cm=21, speed=1),
+            background(
+                Defs.pom_remover_servo.right()
+            ),
             turn_to_heading_right(0),
 
             # wait a little and then remove the blue pom
             background(
                 seq([
-                    wait_for_seconds(1.5),
                     Defs.pom_remover_servo.yeet_blue_pom(),
                     Defs.pom_remover_servo.drum_moving_pos(),
                 ]),
@@ -42,68 +36,16 @@ class M010DriveToDrumsMission(Mission):
             turn_to_heading_left(0),
 
             background(
-                seq([
-                    Defs.pom_remover_servo.drum_moving_pos(),
+                parallel(
                     Defs.lift_drums_servo.down(),
-                ]),
-                name="lower_drum"
+                    Defs.drum_pusher_servo.open(),
+                ),
+                name="lower_drum",
             ),
 
-            wall_align_forward(accel_threshold=0.3),
-            #mark_heading_reference(),
+            wall_align_forward(
+                accel_threshold=0.3,
+                grace_period=0.2,
+            ),
+            mark_heading_reference(),
         ])
-
-
-        # return seq([
-        #     mark_heading_reference(
-        #         origin_offset_deg=90,
-        #     ),
-        #
-        #     background(
-        #         step=seq([
-        #             # push orange pom away
-        #             wait_for_seconds(0.1),
-        #             Defs.pom_remover_servo.orange_pom_removel(),
-        #             fully_disable_servos(),
-        #         ])
-        #     ),
-        #
-        #     #wait_for_seconds(0.3),
-        #
-        #     smooth_path(
-        #         turn_to_heading_right(16.7),  # over rotate so we push the sorted poms better value before 14
-        #         turn_to_heading_right(17.8),  # magic value, so we don't hit the extenal loading dock
-        #         parallel(
-        #             seq([
-        #                 drive_forward().until(
-        #                     after_cm(20) +
-        #                     over_line(Defs.front_right_ir_sensor)
-        #                 ),
-        #             ]),
-        #             background(
-        #                 step=seq([
-        #                     wait_until_distance(5),
-        #                     Defs.pom_remover_servo.right(),
-        #                     fully_disable_servos(),
-        #                     wait_until_distance(29),
-        #                     Defs.pom_remover_servo.left(),
-        #                     #fully_disable_servos(),
-        #                     #wait_until_distance(22),
-        #                     drum_recover_from_over_limit(Defs.lift_drums_servo.up),
-        #                 ]),
-        #             ),
-        #         ),
-        #
-        #         # turn straight
-        #         turn_to_heading_right(0),
-        #
-        #         parallel(
-        #             drive_forward(cm=27, heading=0),
-        #             Defs.drum_pusher_servo.open(),
-        #             seq([
-        #                 wait_until_distance(12), #only a good guess of distance
-        #                 drum_lifting_down(slow_mode=False),
-        #             ]),
-        #         ),
-        #     ),
-        # ])
