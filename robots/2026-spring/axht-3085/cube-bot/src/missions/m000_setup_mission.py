0d1e62c4905c2cfd8df59eb7895cbf30904a0985
Matthias Greil <matthias.greil2411@gmail.com>
2026-06-16 16:03:56 +0200

    changd setup misson to use new stuff

--- robots/2026-spring/axht-3085/cube-bot/src/missions/m000_setup_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m000_setup_mission.py
@@ -1,27 +1,33 @@
 from raccoon import *
 from src.kinematics.arm import arm
 from src.hardware.defs import Defs
-from src.missions.m007_move_to_center_mission import left_lateral_line_follow
 from src.steps.calibrate_analog_drive import calibrate_analog_drive, on_analog_flank
-from src.steps.line_follow_dsl import lateral_follow_line_single, lateral_follow_line_single_free, strafe_follow_line_single
-from src.steps.sample_analog_between_lines import sample_analog_between_lines
+from src.steps.setup_calibration import CalibrationAxis, calibration_gate, collect_drive, collect_ir_set
 
-def line_follow():
-    return strafe_follow_line_single(
-        sensor=Defs.front.left,
-        speed=-1,
-        side=LineSide.RIGHT,
-        kp=0.6,
-        ki=0.3,
-        kd=0.05,
-    )
 
 class M000SetupMission(SetupMission):
     setup_time = 90
 
+    async def pre_start_gate(self, robot) -> None:
+        await calibration_gate().run_step(robot)
+        await robot._pre_start_gate()
+
     def sequence(self) -> Sequential:
         return seq([
-
+            # auto_tune(
+            #     tune_bemf_velocity=True,
+            #     tune_vel_lpf=False,
+            #     tune_static_friction=False,
+            #     tune_firmware_pid=False,
+            #     tune_encoder_cal=True,
+            #     tune_characterize=False,
+            #     tune_velocity=False,
+            #     tune_motion=False,
+            #     tune_tolerances=False,
+            #     motion_axes=["distance", "lateral", "heading"],
+            #     step_confirm=True,
+            #     persist=True,
+            # ),
             pause_setup_timer(),
             fully_disable_servos(),
 
@@ -43,61 +49,41 @@ class M000SetupMission(SetupMission):
                 ])
             ),
 
-            calibrate(
-                distance_cm=70,
-                calibration_sets=["default", "upper"],
+            collect_drive(
+                collect_ir_set(
+                    drive_forward(cm=50),
+                    set_name="default",
+                ),
+                axis=CalibrationAxis.FORWARD,
             ),
-            #---
-            #line_follow().until(
-            #    (over_line(Defs.front.right)
-            #     + after_cm(20))
-            #    | after_seconds(6)
-            #),
-            # drive to line
-            #mark_heading_reference(origin_offset_deg=90),
-            #left_lateral_line_follow().until(
-            #    after_cm(40)
-            #),
-
-            #turn_to_heading_left(0),
-
-            #drive_backward().until(
-            #    on_black(Defs.rear.left)
-            #),
-
-            ## line follow backwards to retrieve spot
-            #drive_forward().until(
-            #    over_line(Defs.rear.left)  # if we ever are over the line this conditio will fix it
-            #    + after_cm(7)
-            #),
-            ## go into correct lateral position for pickup
-            #strafe_right(heading=0).until(
-            #    on_black(Defs.rear.left),
-            #),
-            #---
 
             servo(Defs.arm_elbow, -28),
             servo(Defs.arm_sholder, 25),
 
             wait_for_button("calibrate upper cube"),
-            calibrate_analog_drive(
-                Defs.et_sensor,
-                set_name="upper_cube",
-                speed=-0.4,
-                drive_duration_s=2
+            collect_drive(
+                collect_ir_set(
+                    drive_forward(50, heading=0, speed=0.4),
+                    set_name="upper_cube",
+                )
             ),
 
             wait_for_button("calibrate cube stack"),
-            calibrate_analog_drive(
-                Defs.et_sensor,
-                set_name="cube_stack",
-                speed=-0.4,
-                drive_duration_s=2
+            collect_drive(
+                collect_ir_set(
+                    drive_forward(50, heading=0, speed=0.4),
+                    set_name="cube_stack",
+                )
+            ),
+
+            calibration_gate(
+                require_axes=[CalibrationAxis.FORWARD],
+                require_ir_sets=["default", "upper"],
             ),
 
             wait_for_button("go to strart possiont"),
             mark_heading_reference(),
-            #align on the black line on the right
+            # align on the black line on the right
             strafe_right().until(
                 on_black(Defs.front.right)
             ),
@@ -105,7 +91,7 @@ class M000SetupMission(SetupMission):
                 on_white(Defs.front.right)
                 + after_cm(1)
             ),
-            #aling witht the black line in front
+            # aling witht the black line in front
             drive_forward().until(
                 on_black(Defs.front.left)
             ),
@@ -114,7 +100,6 @@ class M000SetupMission(SetupMission):
                 + after_cm(1)
             ),
             turn_to_heading_right(0),
-
 
             arm.move_angles(-55, 130, -110),
             fully_disable_servos(),
