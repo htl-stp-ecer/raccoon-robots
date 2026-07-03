ddb4258a374b74a9965764453b4d49033cbaabfd
MarioS271 <marioerik.scholze@gmail.com>
2026-07-03 20:03:07 +0200

    fix: remove rubber band from pom pusher step again only runs once and goes to right instead of left position

--- robots/2026-spring/axht-3085/drumbot/src/steps/remove_rubber_band_from_pom_pusher_step.py
+++ robots/2026-spring/axht-3085/drumbot/src/steps/remove_rubber_band_from_pom_pusher_step.py
@@ -2,10 +2,7 @@ from raccoon import *
 from src.hardware.defs import Defs
 
 def remove_rubber_band_from_pom_pusher():
-    return loop_for(
-        seq([
-            Defs.pom_remover_servo.left(),
-            Defs.pom_remover_servo.drum_moving_pos(),
-        ]),
-        iterations=2
-    )
+    return seq([
+        Defs.pom_remover_servo.right(),
+        Defs.pom_remover_servo.drum_moving_pos(),
+    ]),
