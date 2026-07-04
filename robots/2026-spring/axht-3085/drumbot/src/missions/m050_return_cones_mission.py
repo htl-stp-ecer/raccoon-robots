fb0b506aa6da110af34b23bf24d71eaf4d7f439c
MarioS271 <marioerik.scholze@gmail.com>
2026-07-04 21:38:50 +0200

    feat: finish cone driving path

--- robots/2026-spring/axht-3085/drumbot/src/missions/m050_return_cones_mission.py
+++ robots/2026-spring/axht-3085/drumbot/src/missions/m050_return_cones_mission.py
@@ -7,20 +7,39 @@ class M050ReturnConesMission(Mission):
         return seq([
             # turn away and tuck in drum while lowering cone pusher
             parallel(
-                turn_left(45),
+                turn_left(53),
                 seq([
                     wait_for_seconds(0.2),
-                    parallel(
-                        Defs.lift_drums_servo.over_limit(120),
-                        lower_cone_pusher(),
-                    ),
+                    Defs.lift_drums_servo.over_limit(120),
                 ]),
             ),
 
-            drive_backward(speed=0.7).until(
+            lower_cone_pusher(),
+
+            # scoop cones into pusher
+            drive_backward().until(
                 over_line(Defs.front_right_ir_sensor)
                 + after_cm(4)
             ),
 
             turn_to_heading_right(90),
+
+            # start driving to lower starting box
+            line_follow()
+            .single(Defs.rear_left_ir_sensor, LineSide.RIGHT)
+            .move(forward=-1)
+            .correct_angular()
+            .pid(1.5, 0.3, 0.1)
+            .until(
+                over_line(Defs.front_right_ir_sensor)
+                + over_line(Defs.front_right_ir_sensor)
+            ),
+
+            drive_arc_left(
+                radius_cm=50,
+                degrees=55
+            ),
+            drive_forward(20),
+
+
         ])
