66ebf239bf85241d208bfd51c07f4e231d204c07
Matthias Greil <matthias.greil2411@gmail.com>
2026-07-07 21:02:36 +0200

    deleted comments

--- robots/2026-spring/axht-3085/cube-bot/src/missions/m010_move_to_center_mission.py
+++ robots/2026-spring/axht-3085/cube-bot/src/missions/m010_move_to_center_mission.py
@@ -20,27 +20,6 @@ class M010MoveToCenterMission(Mission):
     def sequence(self) -> Step:
         return optimize([
             mark_heading_reference(origin_offset_deg=-90),
-            # background(
-            #    step=seq([
-            #        # make sure sholder and arm are enable before enabeling base so we dont get stuck with the claw
-            #        arm.move_angles(base_deg=0), #only use if we run allone
-            #        arm.move_angles(elbow_deg=-70),
-            #        arm.move_angles(sholder_deg=90),
-            #        Defs.arm_claw.idle(),  # make sure claw is closed
-            #    ]),
-            #    name="prep_arm"
-            # ),
-
-            # drive_forward(heading=-90).until(
-            #    over_line(Defs.front.left)
-            #    + on_black(Defs.front.left)
-            # ),
-
-            ## drive to line
-            # left_lateral_line_follow().until(
-            #    after_cm(30)
-            # ),
-
             optimize([
                 drive_backward(cm=10),
                 spline((-20, 0),(-40, 70), absolute_heading=True,final_heading=0, speed=1),
