d5e671aeab09bf19b0b3bfdc3cef23c736e8306f
MarioS271 <marioerik.scholze@gmail.com>
2026-07-12 17:53:11 -0500

    refactor: lower cone pusher now uses steps instead of direct motor commands (+active instead of passive hold now)

--- robots/2026-spring/axht-3085/drumbot/src/steps/cone_pusher_steps.py
+++ robots/2026-spring/axht-3085/drumbot/src/steps/cone_pusher_steps.py
@@ -1,22 +1,12 @@
-import asyncio
-
 from raccoon import *
 from src.hardware.defs import Defs
 
-CONE_PUSHER_MOTOR_MOVE_TIME = 0.4
-CONE_PUSHER_MOTOR_MOVE_VELOCITY = -1300
-
-class LowerConePusherStep(Step):
-    def __init__(self):
-        super().__init__()
-        self._motor_ref = Defs.cone_pusher_motor
-
-    async def _execute_step(self, robot):
-        self._motor_ref.set_velocity(CONE_PUSHER_MOTOR_MOVE_VELOCITY)
-        await asyncio.sleep(CONE_PUSHER_MOTOR_MOVE_TIME)
-        self._motor_ref.brake()
-
+MOTOR_VELOCITY = 1300
+DOWN_POSITION = -420
 
 @dsl()
 def lower_cone_pusher():
-    return LowerConePusherStep()
+    return seq([
+        move_motor_relative(Defs.cone_pusher_motor, DOWN_POSITION, MOTOR_VELOCITY),
+        motor_brake(Defs.cone_pusher_motor),
+    ])
